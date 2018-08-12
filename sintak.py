100f2f22bf1b60dff13eda29b76a1300f245dad2
stelin <lintangtimur@users.noreply.github.com>
2018-08-12 14:24:32 +0700

    Update sintak.py

--- sintak.py
+++ sintak.py
@@ -9,10 +9,6 @@ try:
 except ImportError:
     exit("[!] wrong installation detected (missing modules)")
 
-# # a = readCsv('MhsAktif_SistemInformasi.csv', ['NIM','TGL LAHIR'])
-# # username = a['NIM'][0]
-# # password = a['TGL LAHIR'][0]
-
 username = '15.N1.0020'
 password = '24/05/199s7'
 
