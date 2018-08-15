01c08078bf14ad3b2f494540fbb64554c9b02833
lintangtimur <lintangtimur915@gmail.com>
2018-08-15 19:36:49 +0700

    rename packages

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/lintangtimur/SintakParser",
-    packages=setuptools.find_packages(),
+    packages=['sintak-unika-parser'],
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
