d885df7394633ee15f99287d6896499fb131540e
lintangtimur <lintangtimur915@gmail.com>
2018-12-18 22:13:20 +0700

    add getKHS(), getTagihan()

--- sintak_parser/Fetch.py
+++ sintak_parser/Fetch.py
@@ -3,10 +3,13 @@ try:
     import os
     import requests
     from sintak_parser.Endpoint import Endpoint, Req_Header
+    from sintak_parser.utils.core.res_message import ResourceMessage
     from sintak_parser.Parser import SintakParser
     from sintak_parser.utils.helper import log
     from colorama import init, Fore, Back, Style
     from sintak_parser.utils.helper import banner
+    from bs4 import BeautifulSoup
+
     init()
     banner()
 except ModuleNotFoundError:
@@ -22,7 +25,9 @@ def checkLogin(func):
         if not args[0].resp == None:
             return func(*args)
         else:
-            print("{}[WARNING]Function {}() can be execute if success login first{}".format(Fore.YELLOW, func.__name__,Style.RESET_ALL))
+            print("{}[WARNING]Function {}() can be execute if success login first{}".format(
+                Fore.YELLOW, func.__name__,Style.RESET_ALL)
+            )
             
     return isLogin
 
@@ -31,6 +36,9 @@ class Fetch():
     Fetch Sintak Data
     """
 
+    EXT_IMG = ".jpg"
+    PREFIX_FILEKTM = "ktm_"
+    
     def __init__(self, username, password):
          
         self.resp = None
@@ -54,12 +62,18 @@ class Fetch():
 
         session = requests.Session()
         self.session = session
-        resp = session.post(Endpoint.SINTAKID_VALIDATE, data=LOGINDATA, headers=Req_Header.HEADERS)
-        if resp.text != 'Username atau Password yang anda masukkan salah.':
+        resp = session.post(
+            Endpoint.SINTAKID_VALIDATE, data=LOGINDATA, headers=Req_Header.HEADERS
+        )
+        if resp.text != ResourceMessage.ALERT['wrongCredentials']:
             self.resp = resp.content
-            print("{}[INFO] {} success login !{}".format(Fore.LIGHTGREEN_EX, username,Style.RESET_ALL))
+            print("{}[INFO] {} success login !{}".format(
+                Fore.LIGHTGREEN_EX, username,Style.RESET_ALL)
+            )
         else:
-            print("{}{}[CRITICAL] {} Failed to login !{}".format(Back.RED,Fore.WHITE, username,Style.RESET_ALL))
+            print("{}{}[CRITICAL] {} Failed to login !{}".format(
+                Back.RED,Fore.WHITE, username,Style.RESET_ALL)
+            )
         
     def IsDashboardAccessible(self):
         """
@@ -91,6 +105,95 @@ class Fetch():
     def getTranskrip(self):
         pass
 
+    
+    def getKHS(self, smt, tahun):
+        """
+        Mendapatkan Kartu Hasil Studi berdasarkan semester dan tahun ajar
+        
+        Args:
+            smt (str): semester
+            tahun (str): tahun ajar, 2015 sampai 2018
+        """
+        tahunAjar = ['2015', '2016', '2017', '2018']
+
+        semester = {
+            "ganjil" : "G1",
+            "genap" : "G2",
+            "sisipan" : "S1"
+        }
+
+        dataPayload = {
+            "smt" : semester[smt],
+            "tahun": tahun
+        }
+        resp = self.session.post(Endpoint.SINTAKID_URL_KHS, data=dataPayload, headers=Req_Header.HEADERS)
+        return resp.json()
+
+    def getTagihan(self, tahun, semester):
+        """
+        Mendapatkan tagihan
+        
+        Args:
+            tahun (str): tahun, contoh 2018,2017
+            semester (str): G1,G2
+        """
+
+        dataPayload = {
+            "th" : tahun,
+            "uji": semester
+        }
+        temp = {
+            "kode_mk" : [],
+            "nama_mk" : [],
+            "sks" : [],
+            "kelas": [],
+            "status": [],
+            "jumlah":[],
+            "total": ""
+        }
+
+        tableBotTemp = {
+            "tagihan" : [],
+            "jumlah": [],
+            "total" : ""
+        }
+
+        resp = self.session.post(Endpoint.SINTAK_URL_TAGIHAN, data=dataPayload, headers=Req_Header.HEADERS).text
+        soup = BeautifulSoup(resp, 'html.parser')
+        tables = soup.findAll("table", {"cellspacing": 2,"cellpadding":2})
+        tableTop = tables[0]
+        tableBot = tables[1]
+
+        trData = tableTop.findAll("tr")
+        trDataBot = tableBot.findAll("tr")
+        
+        #======================================= PARSING TABLE BAGIAN ATAS ============================
+        for i in range(1, len(trData)-1):
+            temp['kode_mk'].append(trData[i].findAll("td")[0].text)
+            temp['nama_mk'].append(trData[i].findAll("td")[1].text)
+            temp['sks'].append(trData[i].findAll("td")[2].text)
+            temp['kelas'].append(trData[i].findAll("td")[3].text)
+            temp['status'].append(trData[i].findAll("td")[4].text)
+            temp['jumlah'].append(self._parseCurrency(trData[i].findAll("td")[5].text))
+        
+        total = self._parseCurrency(trData[len(trData)-1].findAll("td")[1].text)
+        temp['total'] = total
+
+        #======================================= PARSING TABLE BAGIAN BAWAH ===========================
+        for i in range(1, len(trDataBot)-1):
+            tableBotTemp['tagihan'].append(trDataBot[i].findAll("td")[0].text)
+            tableBotTemp['jumlah'].append(self._parseCurrency(trDataBot[i].findAll("td")[1].text))
+
+        totalBot = self._parseCurrency(trDataBot[len(trDataBot)-1].findAll("td")[1].text)
+        tableBotTemp['total'] = totalBot
+        return {
+            "tableTop" : temp,
+            "tableBot" : tableBotTemp
+        }
+
+    def _parseCurrency(self, cash):
+        return cash.split(',')[0].replace('.',"")
+
     @checkLogin
     def getKtm(self, saveTo):
         """
@@ -107,21 +210,21 @@ class Fetch():
             currentDirectory = os.getcwd()
             directoryToCreate = saveTo
             if os.path.exists(currentDirectory+directoryToCreate):
-                print("Exist")
+                print(ResourceMessage.ALERT['directoryExist'])
             else:
                 os.makedirs(directoryToCreate, exist_ok=True)
             
-            fileName = 'ktm_'+self.username+'.jpg'
+            fileName = self.PREFIX_FILEKTM+self.username+self.EXT_IMG
             completePath = os.path.join(currentDirectory+"\\"+directoryToCreate, fileName)
             
             try:
-                    with open(completePath, 'wb') as handler:
-                        handler.write(img_data)
-                # finally:
-                #     handler.close()
+                with open(completePath, 'wb') as handler:
+                    handler.write(img_data)
             except IOError as e:
-                print("error write to file")
+                print(ResourceMessage.ALERT['errorWriteToFile'])
                 print(e)
             finally:
                 handler.close()
-                print("{}[SAVED] {} saved to {}{}".format(Fore.GREEN, fileName, completePath, Style.RESET_ALL))
+                print("{}[SAVED] {} saved to {}{}".format(
+                    Fore.GREEN, fileName, completePath, Style.RESET_ALL)
+                )
